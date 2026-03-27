70f46483e3c4fdce0224df1dd5acfa6143f377b9
Vipransh Ojha <ojhavipransh@gmail.com>
2026-03-27 13:35:36 +0530

    feat: Add script to generate IPL player data from CSV and format it for the application.

--- backend/generate_data.py
+++ backend/generate_data.py
@@ -2,133 +2,250 @@ import json
 
 csv_data = """Team Name,Player Name,Role
 Chennai Super Kings,Ruturaj Gaikwad,BAT
-Chennai Super Kings,MS Dhoni,WK
-Chennai Super Kings,Ravindra Jadeja,AR
-Chennai Super Kings,Matheesha Pathirana,BOW
-Chennai Super Kings,Ravichandran Ashwin,AR
-Chennai Super Kings,Khaleel Ahmed,BOW
-Chennai Super Kings,Rahul Tripathi,BAT
-Chennai Super Kings,Sam Curran,AR
-Chennai Super Kings,Devon Conway,WK
-Chennai Super Kings,Vijay Shankar,AR
-Chennai Super Kings,Rachin Ravindra,AR
-Chennai Super Kings,Mukesh Choudhary,BOW
-Chennai Super Kings,Deepak Hooda,AR
+Chennai Super Kings,Dewald Brevis,BAT
+Chennai Super Kings,Ayush Mhatre,BAT
+Chennai Super Kings,Matthew Short,BAT
+Chennai Super Kings,Sarfaraz Khan,BAT
+Chennai Super Kings,Shivam Dube,AR
+Chennai Super Kings,Ramakrishna Ghosh,AR
 Chennai Super Kings,Jamie Overton,AR
-Chennai Super Kings,Shaik Rasheed,BAT
+Chennai Super Kings,Anshul Kamboj,AR
+Chennai Super Kings,Prashant Veer,AR
+Chennai Super Kings,Zakary Foulkes,AR
+Chennai Super Kings,Aman Khan,AR
+Chennai Super Kings,MS Dhoni,WK
+Chennai Super Kings,Urvil Patel,WK
+Chennai Super Kings,Sanju Samson,WK
+Chennai Super Kings,Kartik Sharma,WK
+Chennai Super Kings,Shreyas Gopal,BOW
+Chennai Super Kings,Khaleel Ahmed,BOW
+Chennai Super Kings,Gurjapneet Singh,BOW
+Chennai Super Kings,Mukesh Choudhary,BOW
+Chennai Super Kings,Noor Ahmad,BOW
+Chennai Super Kings,Akeal Hosein,BOW
+Chennai Super Kings,Matt Henry,BOW
+Chennai Super Kings,Rahul Chahar,BOW
+Chennai Super Kings,Spencer Johnson,BOW
+Delhi Capitals,Karun Nair,BAT
+Delhi Capitals,Nitish Rana,BAT
+Delhi Capitals,Sameer Rizvi,BAT
+Delhi Capitals,David Miller,BAT
+Delhi Capitals,Pathum Nissanka,BAT
+Delhi Capitals,Sahil Parakh,BAT
+Delhi Capitals,Prithvi Shaw,BAT
+Delhi Capitals,Ashutosh Sharma,AR
 Delhi Capitals,Axar Patel,AR
-Delhi Capitals,Kuldeep Yadav,BOW
-Delhi Capitals,Tristan Stubbs,WK
+Delhi Capitals,Ajay Jadav Mandal,AR
+Delhi Capitals,Madhav Tiwari,AR
+Delhi Capitals,Tripurana Vijay,AR
+Delhi Capitals,Auqib Nabi Dar,AR
 Delhi Capitals,Abishek Porel,WK
 Delhi Capitals,KL Rahul,WK
-Delhi Capitals,Mitchell Starc,BOW
-Delhi Capitals,Jake Fraser-McGurk,BAT
-Delhi Capitals,Faf du Plessis,BAT
-Delhi Capitals,T Natarajan,BOW
-Delhi Capitals,Harry Brook,BAT
+Delhi Capitals,Tristan Stubbs,WK
+Delhi Capitals,Dushmantha Chameera,BOW
+Delhi Capitals,Kuldeep Yadav,BOW
 Delhi Capitals,Mukesh Kumar,BOW
-Delhi Capitals,Sameer Rizvi,BAT
-Delhi Capitals,Ashutosh Sharma,BAT
-Gujarat Titans,Rashid Khan,BOW
+Delhi Capitals,T Natarajan,BOW
+Delhi Capitals,Vipraj Nigam,BOW
+Delhi Capitals,Mitchell Starc,BOW
+Delhi Capitals,Lungi Ngidi,BOW
+Delhi Capitals,Kyle Jamieson,BOW
 Gujarat Titans,Shubman Gill,BAT
+Gujarat Titans,M Shahrukh Khan,BAT
 Gujarat Titans,Sai Sudharsan,BAT
+Gujarat Titans,Nishant Sindhu,AR
+Gujarat Titans,Rashid Khan,AR
+Gujarat Titans,Manav Suthar,AR
 Gujarat Titans,Rahul Tewatia,AR
-Gujarat Titans,Shahrukh Khan,BAT
-Gujarat Titans,Kagiso Rabada,BOW
-Gujarat Titans,Mohammed Siraj,BOW
 Gujarat Titans,Washington Sundar,AR
-Gujarat Titans,Prasidh Krishna,BOW
+Gujarat Titans,Arshad Khan,AR
+Gujarat Titans,Ravisrinivasan Sai Kishore,AR
+Gujarat Titans,Jason Holder,AR
+Gujarat Titans,Anuj Rawat,WK
 Gujarat Titans,Jos Buttler,WK
+Gujarat Titans,Kumar Kushagra,WK
 Gujarat Titans,Glenn Phillips,WK
-Gujarat Titans,Ishan Kishan,WK
-Gujarat Titans,Gerald Coetzee,BOW
-Kolkata Knight Riders,Rinku Singh,BAT
-Kolkata Knight Riders,Varun Chakaravarthy,BOW
-Kolkata Knight Riders,Sunil Narine,AR
-Kolkata Knight Riders,Andre Russell,AR
-Kolkata Knight Riders,Harshit Rana,BOW
-Kolkata Knight Riders,Ramandeep Singh,AR
-Kolkata Knight Riders,Venkatesh Iyer,AR
-Kolkata Knight Riders,Quinton de Kock,WK
-Kolkata Knight Riders,Anrich Nortje,BOW
-Kolkata Knight Riders,Manish Pandey,BAT
-Kolkata Knight Riders,Rahmanullah Gurbaz,WK
-Kolkata Knight Riders,Rovman Powell,BAT
-Lucknow Super Giants,Nicholas Pooran,WK
-Lucknow Super Giants,Ravi Bishnoi,BOW
-Lucknow Super Giants,Mayank Yadav,BOW
-Lucknow Super Giants,Mohsin Khan,BOW
-Lucknow Super Giants,Ayush Badoni,BAT
-Lucknow Super Giants,Rishabh Pant,WK
-Lucknow Super Giants,David Miller,BAT
-Lucknow Super Giants,Aiden Markram,BAT
-Lucknow Super Giants,Mitchell Marsh,AR
-Lucknow Super Giants,Abdul Samad,BAT
-Lucknow Super Giants,Avesh Khan,BOW
-Lucknow Super Giants,Akash Deep,BOW
-Mumbai Indians,Jasprit Bumrah,BOW
-Mumbai Indians,Suryakumar Yadav,BAT
-Mumbai Indians,Hardik Pandya,AR
-Mumbai Indians,Rohit Sharma,BAT
-Mumbai Indians,Tilak Varma,BAT
-Mumbai Indians,Trent Boult,BOW
-Mumbai Indians,Naman Dhir,AR
-Mumbai Indians,Robin Minz,WK
-Mumbai Indians,Will Jacks,AR
-Mumbai Indians,Ryan Rickelton,WK
-Mumbai Indians,Karn Sharma,BOW
-Mumbai Indians,Deepak Chahar,BOW
-Mumbai Indians,Mitchell Santner,AR
-Punjab Kings,Shashank Singh,BAT
-Punjab Kings,Prabhsimran Singh,WK
-Punjab Kings,Shreyas Iyer,BAT
-Punjab Kings,Arshdeep Singh,BOW
-Punjab Kings,Yuzvendra Chahal,BOW
-Punjab Kings,Marcus Stoinis,AR
-Punjab Kings,Glenn Maxwell,AR
-Punjab Kings,Nehal Wadhera,BAT
-Punjab Kings,Harshal Patel,BOW
-Punjab Kings,Aaron Hardie,AR
-Punjab Kings,Marco Jansen,AR
-Punjab Kings,Piyush Chawla,BOW
-Punjab Kings,Josh Inglis,WK
-Rajasthan Royals,Sanju Samson,WK
-Rajasthan Royals,Yashasvi Jaiswal,BAT
-Rajasthan Royals,Riyan Parag,BAT
-Rajasthan Royals,Dhruv Jurel,WK
-Rajasthan Royals,Shimron Hetmyer,BAT
-Rajasthan Royals,Sandeep Sharma,BOW
-Rajasthan Royals,Jofra Archer,BOW
-Rajasthan Royals,Maheesh Theekshana,BOW
-Rajasthan Royals,Wanindu Hasaranga,AR
-Rajasthan Royals,Nitish Rana,BAT
-Rajasthan Royals,Tushar Deshpande,BOW
-Rajasthan Royals,Akash Madhwal,BOW
-Rajasthan Royals,Fazalhaq Farooqi,BOW
-Royal Challengers Bengaluru,Virat Kohli,BAT
+Gujarat Titans,Tom Banton,WK
+Gujarat Titans,Gurnoor Brar,BOW
+Gujarat Titans,Mohammed Siraj,BOW
+Gujarat Titans,Prasidh Krishna,BOW
+Gujarat Titans,Kagiso Rabada,BOW
+Gujarat Titans,Jayant Yadav,BOW
+Gujarat Titans,Ishant Sharma,BOW
+Gujarat Titans,Ashok Sharma,BOW
+Gujarat Titans,Luke Wood,BOW
+Gujarat Titans,Kulwant Khejroliya,BOW
 Royal Challengers Bengaluru,Rajat Patidar,BAT
-Royal Challengers Bengaluru,Yash Dayal,BOW
-Royal Challengers Bengaluru,Phil Salt,WK
-Royal Challengers Bengaluru,Jitesh Sharma,WK
-Royal Challengers Bengaluru,Liam Livingstone,AR
-Royal Challengers Bengaluru,Josh Hazlewood,BOW
-Royal Challengers Bengaluru,Rasikh Salam,BOW
-Royal Challengers Bengaluru,Suyash Sharma,BOW
-Royal Challengers Bengaluru,Krunal Pandya,AR
-Royal Challengers Bengaluru,Bhuvneshwar Kumar,BOW
 Royal Challengers Bengaluru,Tim David,BAT
+Royal Challengers Bengaluru,Virat Kohli,BAT
 Royal Challengers Bengaluru,Devdutt Padikkal,BAT
-Sunrisers Hyderabad,Heinrich Klaasen,WK
-Sunrisers Hyderabad,Pat Cummins,BOW
-Sunrisers Hyderabad,Abhishek Sharma,AR
+Royal Challengers Bengaluru,Jacob Bethell,AR
+Royal Challengers Bengaluru,Krunal Pandya,AR
+Royal Challengers Bengaluru,Venkatesh Iyer,AR
+Royal Challengers Bengaluru,Vihaan Malhotra,AR
+Royal Challengers Bengaluru,Romario Shepherd,AR
+Royal Challengers Bengaluru,Mangesh Yadav,AR
+Royal Challengers Bengaluru,Kanishk Chouhan,AR
+Royal Challengers Bengaluru,Satvik Deswal,AR
+Royal Challengers Bengaluru,Philip Salt,WK
+Royal Challengers Bengaluru,Jitesh Sharma,WK
+Royal Challengers Bengaluru,Jordan Cox,WK
+Royal Challengers Bengaluru,Abhinandan Singh,BOW
+Royal Challengers Bengaluru,Josh Hazlewood,BOW
+Royal Challengers Bengaluru,Rasikh Salam Dar,BOW
+Royal Challengers Bengaluru,Bhuvneshwar Kumar,BOW
+Royal Challengers Bengaluru,Suyash Sharma,BOW
+Royal Challengers Bengaluru,Swapnil Singh,BOW
+Royal Challengers Bengaluru,Nuwan Thushara,BOW
+Royal Challengers Bengaluru,Jacob Duffy,BOW
+Royal Challengers Bengaluru,Vicky Ostwal,BOW
+Punjab Kings,Shreyas Iyer,BAT
+Punjab Kings,Priyansh Arya,BAT
+Punjab Kings,Pyla Avinash,BAT
+Punjab Kings,Harnoor Singh,BAT
+Punjab Kings,Nehal Wadhera,BAT
+Punjab Kings,Mitchell Owen,AR
+Punjab Kings,Musheer Khan,AR
+Punjab Kings,Shashank Singh,AR
+Punjab Kings,Marcus Stoinis,AR
+Punjab Kings,Suryansh Shedge,AR
+Punjab Kings,Cooper Connolly,AR
+Punjab Kings,Azmatullah Omarzai,AR
+Punjab Kings,Marco Jansen,AR
+Punjab Kings,Praveen Dubey,AR
+Punjab Kings,Prabhsimran Singh,WK
+Punjab Kings,Vishnu Vinod,WK
+Punjab Kings,Arshdeep Singh,BOW
+Punjab Kings,Xavier Bartlett,BOW
+Punjab Kings,Yuzvendra Chahal,BOW
+Punjab Kings,Lockie Ferguson,BOW
+Punjab Kings,Harpreet Brar,BOW
+Punjab Kings,Vijaykumar Vyshak,BOW
+Punjab Kings,Yash Thakur,BOW
+Punjab Kings,Ben Dwarshuis,BOW
+Punjab Kings,Vishal Nishad,BOW
+Kolkata Knight Riders,Angkrish Raghuvanshi,BAT
+Kolkata Knight Riders,Rinku Singh,BAT
+Kolkata Knight Riders,Finn Allen,BAT
+Kolkata Knight Riders,Rahul Tripathi,BAT
+Kolkata Knight Riders,Sarthak Ranjan,BAT
+Kolkata Knight Riders,Ramandeep Singh,AR
+Kolkata Knight Riders,Anukul Roy,AR
+Kolkata Knight Riders,Cameron Green,AR
+Kolkata Knight Riders,Rachin Ravindra,AR
+Kolkata Knight Riders,Sunil Narine,AR
+Kolkata Knight Riders,Daksh Kamra,AR
+Kolkata Knight Riders,Tim Seifert,WK
+Kolkata Knight Riders,Tejasvi Dahiya,WK
+Kolkata Knight Riders,Vaibhav Arora,BOW
+Kolkata Knight Riders,Umran Malik,BOW
+Kolkata Knight Riders,Varun Chakaravarthy,BOW
+Kolkata Knight Riders,Prashant Solanki,BOW
+Kolkata Knight Riders,Kartik Tyagi,BOW
+Kolkata Knight Riders,Matheesha Pathirana,BOW
+Kolkata Knight Riders,Blessing Muzarabani,BOW
+Kolkata Knight Riders,Saurabh Dubey,BOW
+Kolkata Knight Riders,Navdeep Saini,BOW
 Sunrisers Hyderabad,Travis Head,BAT
+Sunrisers Hyderabad,Smaran Ravichandran,BAT
+Sunrisers Hyderabad,Aniket Verma,BAT
+Sunrisers Hyderabad,Abhishek Sharma,AR
+Sunrisers Hyderabad,Kamindu Mendis,AR
 Sunrisers Hyderabad,Nitish Kumar Reddy,AR
-Sunrisers Hyderabad,Mohammed Shami,BOW
+Sunrisers Hyderabad,Liam Livingstone,AR
+Sunrisers Hyderabad,Harsh Dubey,AR
+Sunrisers Hyderabad,Shivang Kumar,AR
 Sunrisers Hyderabad,Ishan Kishan,WK
+Sunrisers Hyderabad,Heinrich Klaasen,WK
+Sunrisers Hyderabad,Salil Arora,WK
+Sunrisers Hyderabad,Brydon Carse,BOW
+Sunrisers Hyderabad,Pat Cummins,BOW
+Sunrisers Hyderabad,Eshan Malinga,BOW
+Sunrisers Hyderabad,Jaydev Unadkat,BOW
 Sunrisers Hyderabad,Harshal Patel,BOW
-Sunrisers Hyderabad,Rahul Chahar,BOW
-Sunrisers Hyderabad,Adam Zampa,BOW
-Sunrisers Hyderabad,Abhinav Manohar,BAT
+Sunrisers Hyderabad,Zeeshan Ansari,BOW
+Sunrisers Hyderabad,Sakib Hussain,BOW
+Sunrisers Hyderabad,Omkar Tukaram Tarmale,BOW
+Sunrisers Hyderabad,Amit Kumar,BOW
+Sunrisers Hyderabad,Praful Hinge,BOW
+Sunrisers Hyderabad,Krish Fuletra,BOW
+Sunrisers Hyderabad,Shivam Mavi,BOW
+Sunrisers Hyderabad,David Payne,BOW
+Team Name,Player Name,Role
+Rajasthan Royals,Shubham Dubey,BAT
+Rajasthan Royals,Shimron Hetmyer,BAT
+Rajasthan Royals,Yashasvi Jaiswal,BAT
+Rajasthan Royals,Vaibhav Sooryavanshi,BAT
+Rajasthan Royals,Aman Rao Perala,BAT
+Rajasthan Royals,Riyan Parag,AR
+Rajasthan Royals,Dasun Shanaka,AR
+Rajasthan Royals,Ravindra Jadeja,AR
+Rajasthan Royals,Dhruv Jurel,WK
+Rajasthan Royals,Lhuan-dre Pretorius,WK
+Rajasthan Royals,Donovan Ferreira,WK
+Rajasthan Royals,Ravi Singh,WK
+Rajasthan Royals,Jofra Archer,BOW
+Rajasthan Royals,Nandre Burger,BOW
+Rajasthan Royals,Tushar Deshpande,BOW
+Rajasthan Royals,Kwena Maphaka,BOW
+Rajasthan Royals,Sandeep Sharma,BOW
+Rajasthan Royals,Yudhvir Singh Charak,BOW
+Rajasthan Royals,Vignesh Puthur,BOW
+Rajasthan Royals,Yash Raj Punja,BOW
+Rajasthan Royals,Sushant Mishra,BOW
+Rajasthan Royals,Ravi Bishnoi,BOW
+Rajasthan Royals,Brijesh Sharma,BOW
+Rajasthan Royals,Adam Milne,BOW
+Rajasthan Royals,Kuldeep Sen,BOW
+Lucknow Super Giants,Himmat Singh,BAT
+Lucknow Super Giants,Aiden Markram,BAT
+Lucknow Super Giants,Akshat Raghuwanshi,BAT
+Lucknow Super Giants,Abdul Samad,AR
+Lucknow Super Giants,Ayush Badoni,AR
+Lucknow Super Giants,Arshin Kulkarni,AR
+Lucknow Super Giants,Mitchell Marsh,AR
+Lucknow Super Giants,Shahbaz Ahmed,AR
+Lucknow Super Giants,Arjun Sachin Tendulkar,AR
+Lucknow Super Giants,Wanindu Hasaranga,AR
+Lucknow Super Giants,Rishabh Pant,WK
+Lucknow Super Giants,Matthew Breetzke,WK
+Lucknow Super Giants,Nicholas Pooran,WK
+Lucknow Super Giants,Mukul Choudhary,WK
+Lucknow Super Giants,Josh Inglis,WK
+Lucknow Super Giants,Akash Maharaj Singh,BOW
+Lucknow Super Giants,Avesh Khan,BOW
+Lucknow Super Giants,Mohammed Shami,BOW
+Lucknow Super Giants,Prince Yadav,BOW
+Lucknow Super Giants,Mohsin Khan,BOW
+Lucknow Super Giants,Digvesh Singh Rathi,BOW
+Lucknow Super Giants,Manimaran Siddharth,BOW
+Lucknow Super Giants,Mayank Prabhu Yadav,BOW
+Lucknow Super Giants,Anrich Nortje,BOW
+Lucknow Super Giants,Naman Tiwari,BOW
+Mumbai Indians,Rohit Sharma,BAT
+Mumbai Indians,Suryakumar Yadav,BAT
+Mumbai Indians,Tilak Varma,BAT
+Mumbai Indians,Danish Malewar,BAT
+Mumbai Indians,Sherfane Rutherford,AR
+Mumbai Indians,Hardik Pandya,AR
+Mumbai Indians,Raj Bawa,AR
+Mumbai Indians,Will Jacks,AR
+Mumbai Indians,Mayank Rawat,AR
+Mumbai Indians,Corbin Bosch,AR
+Mumbai Indians,Mitchell Santner,AR
+Mumbai Indians,Shardul Thakur,AR
+Mumbai Indians,Atharva Ankolekar,AR
+Mumbai Indians,Robin Minz,WK
+Mumbai Indians,Ryan Rickelton,WK
+Mumbai Indians,Quinton de Kock,WK
+Mumbai Indians,Trent Boult,BOW
+Mumbai Indians,Ashwani Kumar,BOW
+Mumbai Indians,Jasprit Bumrah,BOW
+Mumbai Indians,Deepak Chahar,BOW
+Mumbai Indians,Mayank Markande,BOW
+Mumbai Indians,AM Ghazanfar,BOW
+Mumbai Indians,Raghu Sharma,BOW
+Mumbai Indians,Mohammed Salahuddin Izhar,BOW
 """
 
 TEAMS_MAP = {
